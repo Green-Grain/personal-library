40ecf227ad7d5daf883723ccc6c7179594fb9ff9
Green-Grain <zwei.blattes@gmail.com>
2019-12-23 19:05:17 +0900

    自動deploy try&error

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'personal-library'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'https://github.com/Green-Grain/personal-library.git'
+set :repo_url,  'git@github.com:Green-Grain/personal-library.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
