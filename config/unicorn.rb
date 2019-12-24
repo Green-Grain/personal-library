949fdb363e8e0a7045526c363321224ea2efc545
Green-Grain <zwei.blattes@gmail.com>
2019-12-24 15:56:49 +0900

    try&error

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,8 +11,8 @@ working_directory "#{app_path}/current"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-
+# listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+listen 443
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
